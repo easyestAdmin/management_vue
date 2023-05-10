7616b198fe6dd91f9aa0ee5cdfbe5a68ed3cbd49
张迪生 <2046353594@qq.com>
2023-05-10 16:05:52 +0800

    fix

--- src/view/login.vue
+++ src/view/login.vue
@@ -4,6 +4,13 @@
 
 <script lang="ts" setup>
 import { login } from "@/http/login";
-login({});
+const loginManage = async () => {
+  const { data } = await login({
+    username: "鸡哥哥",
+    password: "1234",
+  });
+  console.log(data);
+};
+loginManage();
 </script>
 <style lang="less"></style>
